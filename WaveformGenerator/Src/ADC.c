027c325227a76199b472a3be1ea93023772fe53d
19438926 <1204183324@qq.com>
2023-11-13 19:32:37 +0000

    ADC part2
    ADC read DAC signals

--- WaveformGenerator/Src/ADC.c
+++ WaveformGenerator/Src/ADC.c
@@ -37,6 +37,10 @@ float RawTemperature;
 uint32_t ADC_Temperature_Output;
 uint8_t uc_New_Temp_Data_Ready= FALSE;
 
+float fADC;
+uint32_t ADC_Analogue_Value;
+uint32_t ul_ADC_Averaging = 10;
+
 /**************************/
 /* Local only function prototypes */
 
@@ -50,6 +54,8 @@ uint8_t uc_New_Temp_Data_Ready= FALSE;
  */
 void ADC_Init(void)
 {
+	///////////////////////////////////
+	// Enable temperature reading
 	// Enable ADC1 clock
 	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 
@@ -76,6 +82,37 @@ void ADC_Init(void)
 
 	// Trigger conversion start
 	ADC1->CR2 |= ADC_CR2_SWSTART;
+
+	/////////////////////////////////////
+	// Initialise the ADC Input reading
+	// GPIO Config
+	// Enable the GPIOF clock
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN;
+
+	// Configure PF6 as an analogue input.
+	GPIOF->MODER |= (GPIO_MODER_MODER6_0 | GPIO_MODER_MODER6_1);
+
+	// ADC Config
+	// Enable the ADC3 clock
+	RCC->APB2ENR |= RCC_APB2ENR_ADC3EN;
+
+	// Configure the ADC3 number of channels to convert
+	ADC3->SQR1 &= ~(ADC_SQR1_L_0 | ADC_SQR1_L_1 | ADC_SQR1_L_2 | ADC_SQR1_L_3);
+
+	// Configure what channel's to be read: IN4
+	ADC3->SQR3 |= ADC_SQR3_SQ1_2;
+
+	// Set sample time - as slow as possible(480 cycles).
+	ADC3->SMPR2 |= ADC_SMPR2_SMP4_0 | ADC_SMPR2_SMP4_1 | ADC_SMPR2_SMP4_2;
+
+	// Set for continuous readings
+	ADC3->CR2 |= ADC_CR2_CONT;
+
+	// Start the ADC
+	ADC3->CR2 |= ADC_CR2_ADON;
+
+	// Trigger initial conversion
+	ADC3->CR2 |= ADC_CR2_SWSTART;
 }
 
 /*********************************************
@@ -122,6 +159,15 @@ void ADC_Run(void)
 		RawTemperature = (((float)((3*(float)ADC_Temperature_Output)/(float)4096) - 0.76) / 0.0025) + 25;
 		// Also calculate a 'Rolling Average' (filtered value).
 		FilteredTemerature += ((RawTemperature - FilteredTemerature)*0.0001); // Average over 10000 readings.
+	}
+
+	// Has the Analogue input conversion completed?
+	if (ADC3->SR & ADC_SR_EOC)
+	{
+		// Simple Rolling Average of input value using floating point.
+		fADC = (((fADC * (ul_ADC_Averaging - 1)) + ADC3->DR) / ul_ADC_Averaging);
+		// Update our 32 bit value from floating point number.
+		ADC_Analogue_Value = (uint32_t)fADC;
 	}
 
 }
