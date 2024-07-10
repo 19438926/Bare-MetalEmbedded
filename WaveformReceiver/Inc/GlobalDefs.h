df99dd0d9de7561d4d31f618526f7dd80b1ce643
19438926 <1204183324@qq.com>
2024-07-10 13:49:15 +0100

    update

---
+++ WaveformReceiver/Inc/GlobalDefs.h
@@ -0,0 +1,34 @@
+/**************************************************************************************************************
+ *                  File Name       :GlobalDefs.h
+ *                  Overview        :Central place to keep all global definitions and types etc.
+ *                  Authors         :Wenze Pan
+ **************************************************************************************************************
+ *                  Copyright @ As Applicable
+ **************************************************************************************************************/
+
+#ifndef GLOBALDEFS_H_
+#define GLOBALDEFS_H_
+/***************************************************/
+/* Global Definitions  */
+#define          SYS_CLOCK_FRQ              16000000
+#define          APB1_TIMER_CLOCK_FRQ       16000000
+#define          APB2_CLOCK_FRQ             16000000
+
+#ifndef  FALSE
+#define  FALSE   0
+#endif
+#ifndef  TRUE
+#define  TRUE   !FALSE
+#endif
+
+/***************************************************/
+/* Global Types */
+
+/**************************************************/
+/* Globally available variable */
+
+/*************************************************/
+/*Globally available functions */
+
+
+#endif /* GLOBALDEFS_H_ */
--- WaveformReceiver/Src/main.c
+++ WaveformReceiver/Src/main.c
@@ -14,6 +14,10 @@
 /*Required Header Files */
 #include "stm32f429xx.h"
 
+#include "GlobalDefs.h"
+#include "SysTick.h"
+#include "CAN.h"
+
 
 
 /****************************************************/
@@ -37,7 +41,7 @@
 /* Local function prototypes */
 void Micro_Initialisation(void);
 void Initialise_GPIO(void);
-void Initialise_External_Clock(void);
+void Initialise_Internal_Clock(void);
 
 
 /*********************************************
@@ -48,6 +52,90 @@ void Initialise_External_Clock(void);
  */
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+	//Initialise to use external clock , enable relevant gpio and peripheral etc.
+	Micro_Initialisation();
+
+	static uint64_t timestamp;
+
+	/******************/
+	/* Loop forever */
+	while (1)
+	{
+		while(SysTick_Elapsed_MicroSeconds(timestamp)<2000000)
+		{
+			GPIOG->ODR &= 0<<13;
+		}
+		timestamp = SysTick_Get_Timestamp();
+		while(SysTick_Elapsed_MicroSeconds(timestamp)<2000000)
+		{
+		GPIOG->ODR |= 1<<13;
+		}
+		timestamp = SysTick_Get_Timestamp();
+	}
+
+}
+
+/***********************************************
+ * @brief   Micro_Initialisation
+ * This function configures the micro clocks ,gpio and peripherals used by the system
+ * @param None
+ * @retval None
+ */
+void Micro_Initialisation(void)
+{
+	//configure chip to use internal clock source.
+	Initialise_Internal_Clock();
+
+	//Initialise SysTick Interrupt.
+	SysTick_Init();
+
+	//Initialise any GPIO required.
+	Initialise_GPIO();
+
+	//Enable the FPU (floating point co-processor,access = full access)
+	SCB->CPACR |= FPU_CP10_FULL | FPU_CP11_FULL ;
+}
+
+/***********************************************
+ * @brief Initialise Internal Clock
+ * This function configures the mirco to use Internal 16MHz clock source(HSI)
+ * @param None
+ * @retval None
+ */
+void Initialise_Internal_Clock(void)
+{
+	//Set the power enable clock and voltage regulator
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	PWR->CR |= PWR_CR_VOS;
+
+	//Configure the FLASH Pre-fetch and Latency related settings
+	FLASH->ACR |= FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN
+			| FLASH_ACR_LATENCY_0WS;
+}
+
+/********************************************
+ * @Brief Initialise_GPIO
+ * This function initialises any GPIO used by the system.
+ *
+ * Currently this includes;
+ *
+ * PG13: Green LED.
+ *
+ * @param None
+ * @retval None
+ */
+void Initialise_GPIO(void)
+{
+	//Enable clock access to GPIOG and GPIOA.
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOAEN;
+
+	//Set PG13 as output
+	GPIOG->MODER |= (1 << 26);
+	GPIOG->MODER &= ~(1 << 27);
+
+	//CAN : Configure the pin we need(PA11(CAN1_RX)/PA12(CAN1_TX)) for relevant alternate function.
+	GPIOA->MODER |= GPIO_MODER_MODE11_1;
+	GPIOA->MODER |= GPIO_MODER_MODE12_1;
+	GPIOA->AFR[1] |= GPIO_AFRH_AFRH3_0 | GPIO_AFRH_AFRH3_3;
+	GPIOA->AFR[1] |= GPIO_AFRH_AFRH4_0 | GPIO_AFRH_AFRH4_3;
 }
